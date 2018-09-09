9a1efbc81c96cf3ae4e92f930693bb8bbee660ce
perlancar (on netbook-zenbook-ux305) <perlancar@gmail.com>
2018-09-09 07:56:02 +0700

    Initial commit

---
+++ lib/App/nrand.pm
@@ -0,0 +1,20 @@
+package App::nrand;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: Generate normally distributed random numbers
+
+=head1 SYNOPSIS
+
+See the command-line script L<nrand>.
+
+
+=head1 SEE ALSO
+
+L<Math::Random::NormalDistribution>
+
+L<Math::Random::GaussianRange>
+
+=cut
